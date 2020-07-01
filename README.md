442fdb945aaf1b5f79b19b4ae80e6caebdcde86e
XPoet <i@xpoet.cn>
2020-07-01 16:58:27 +0800

    install element-ui and axios

--- README.md
+++ README.md
@@ -17,3 +17,20 @@ npm run build
 
 ### Customize configuration
 See [Configuration Reference](https://cli.vuejs.org/config/).
+
+## 项目依赖
+
+### Element UI
+
+```js
+import ElementUI from 'element-ui'
+import "element-ui/lib/theme-chalk/index.css"
+Vue.use(ElementUI)
+```
+
+### Axios
+
+```js
+import Axios from "axios";
+Vue.prototype.$axios = Axios;
+```
--- src/main.js
+++ src/main.js
@@ -3,10 +3,20 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+// 导入 ElementUI
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+
+// 导入 Axios
+import Axios from "axios";
+
 Vue.config.productionTip = false
 
+Vue.use(ElementUI);
+Vue.prototype.$axios = Axios;
+
 new Vue({
-  router,
-  store,
-  render: h => h(App)
+    router,
+    store,
+    render: h => h(App)
 }).$mount('#app')
