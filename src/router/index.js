f07e325ab714f68c3643f14102eecae0b67857d7
XPoet <i@xpoet.cn>
2020-07-02 16:22:25 +0800

    修改路由配置文件

--- src/router/index.js
+++ src/router/index.js
@@ -8,51 +8,43 @@ import NoteEdit from '../views/NoteEdit'
 Vue.use(VueRouter)
 
 const routes = [
-    {
-        path: '/',
-        name: 'Index',
-        redirect: {name: 'Notes'}
-    },
-    {
-        path: '/notes',
-        name: 'Notes',
-        component: Notes
-    },
-    {
-        path: '/login',
-        name: 'Login',
-        component: Login
-    },
-    {
-        path: '/note/add',
-        name: 'NoteEdit',
-        component: NoteEdit
-    },
-    {
-        path: '/note/:noteId',
-        name: 'NoteDetail',
-        component: NoteDetail
-    },
-    {
-        path: '/note/:id/edit',
-        name: 'NoteEdit',
-        component: NoteEdit
-    },
-    /*
-    // 路由懒加载模式
-    {
-      path: '/about',
-      name: 'About',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/!* webpackChunkName: "about" *!/ '../views/About.vue')
-    }
-    */
+  {
+    path: '/',
+    name: 'Index',
+    redirect: {name: 'Notes'}
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: Login
+  },
+  {
+    path: '/notes',
+    name: 'Notes',
+    // 懒加载
+    component: () => import('../views/Notes')
+  },
+  {
+    path: '/note/add',
+    name: 'NoteEdit',
+    component: NoteEdit
+  },
+  {
+    path: '/note/:noteId',
+    name: 'NoteDetail',
+    component: NoteDetail
+  },
+  {
+    path: '/note/:id/edit',
+    name: 'NoteEdit',
+    component: NoteEdit
+  },
 ]
 
 const router = new VueRouter({
-    routes
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes
 })
 
 export default router
