da04541be7e55719b4c31b43dc581cbe2abfe04b
XPoet <i@xpoet.cn>
2020-07-03 10:30:12 +0800

    完成 Notes 页面开发

--- src/views/Notes.vue
+++ src/views/Notes.vue
@@ -1,7 +1,34 @@
 <template>
-  <div>
+  <div class="notes-container">
     <Header></Header>
-    <h1>Notes</h1>
+    <div class="notes-list">
+      <el-timeline>
+        <el-timeline-item
+          v-for="item in notes"
+          :timestamp="item.created"
+          placement="top"
+        >
+          <el-card>
+            <h4>
+              <!-- 路由跳转到 note 详情页 -->
+              <router-link :to="{name: 'NoteDetail', params: {noteId: item.id}}">
+                {{ item.title }}
+              </router-link>
+            </h4>
+            <p>{{ item.description }}</p>
+          </el-card>
+        </el-timeline-item>
+      </el-timeline>
+    </div>
+    <el-pagination
+      background
+      layout="prev, pager, next"
+      :total="total"
+      :page-size="pageSize"
+      :current-page="currentPage"
+      @current-change="currentChange($event)"
+    >
+    </el-pagination>
   </div>
 </template>
 
@@ -13,11 +40,53 @@
 
     components: {
       Header
+    },
+
+    data() {
+      return {
+        notes: {},
+        currentPage: 1,
+        total: 0,
+        pageSize: 5,
+      }
+    },
+
+    mounted() {
+      this.page(this.currentPage);
+    },
+
+    methods: {
+      page(currentPage) {
+        const _this = this;
+        this.$axios.get(`/notes?currentPage=${currentPage}`)
+          .then(res => {
+            _this.notes = res.data.data['records'];
+            _this.total = res.data.data['total'];
+            _this.pageSize = res.data.data['size'];
+            _this.currentPage = res.data.data['current'];
+          })
+      },
+
+      currentChange(e) {
+        this.page(e);
+      }
     }
 
   }
 </script>
 
-<style scoped>
+<style scoped lang="scss">
+
+  .notes-container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+    .notes-list {
+      width: 80%;
+    }
+
+  }
+
 
 </style>
